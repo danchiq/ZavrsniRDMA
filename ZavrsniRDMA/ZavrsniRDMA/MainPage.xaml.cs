68015fe67f696b4cc5d4f5087b3ba3b52bce41d2
Daniela <Daniela@DESKTOP-UGI7VSK>
2022-08-04 15:56:38 +0200

    Add project files.

---
+++ ZavrsniRDMA/ZavrsniRDMA/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ZavrsniRDMA
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(userEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passEntry.Text);
+            if (!isEmailEmpty && !isPasswordEmpty)
+            {
+                //NAvigate
+            }
+            else
+            {
+                //do not navigate
+            }
+        }
+    }
+}
